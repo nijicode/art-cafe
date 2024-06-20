e544b5065b28401bb6219911d96fb6ca7aa1464d
Aldous <estacioaldous@gmail.com>
2024-06-20 21:06:50 +0800

    test

--- src/components/Hot.jsx
+++ src/components/Hot.jsx
@@ -6,16 +6,26 @@ const Hot = ({ id, img, name, lPrice, mPrice }) => {
       id={id}
       className="bg-white duration-500 hover:scale-105 relative p-5 rounded-lg w-full h-full text-black shadow-lg shadow-gray-700"
     >
-      <img
-        src={img}
-        alt=""
-        className="h-[220px] mb-2 w-full object-cover border-2 object-center rounded-lg"
-      />
+      <div className="relative">
+        <img
+          src={img}
+          alt=""
+          className="h-[220px] mb-2 w-full object-cover border-2 object-center rounded-lg"
+        />
+        <div className="absolute top-0 left-0">
+          <div>
+            <ion-icon name="heart"></ion-icon>
+          </div>
+          <div>
+            <ion-icon name="heart"></ion-icon>
+          </div>
+        </div>
+      </div>
       <h2 className="font-bold text-center uppercase">{name}</h2>
       <div className="flex justify-around items-center">
         <table className="">
           <tr>
-            <td>Medium</td>
+            <td>Medeium</td>
             <td>|</td>
             <td>Large</td>
           </tr>
